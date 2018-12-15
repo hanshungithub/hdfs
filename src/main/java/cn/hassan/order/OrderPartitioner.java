6f781c6eeda4627d5248a5c2832c32a728f68926
hassan <hassanbox@qq.com>
2018-12-15 15:41:59 +0800

    分组

---
+++ src/main/java/cn/hassan/order/OrderPartitioner.java
@@ -0,0 +1,10 @@
+package cn.hassan.order;
+
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.mapreduce.Partitioner;
+
+public class OrderPartitioner extends Partitioner<OrderBean, NullWritable> {
+    public int getPartition(OrderBean orderBean, NullWritable nullWritable, int numPartitions) {
+        return (int) ((orderBean.getOrderId() & Integer.MAX_VALUE) % numPartitions);
+    }
+}
