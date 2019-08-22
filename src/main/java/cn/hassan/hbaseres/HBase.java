64a39c1d36c066679b2e60d38259c0e456e2c84b
hanshungithub <hshanshun@163.com>
2019-08-22 20:03:13 +0800

    crud

--- src/main/java/cn/hassan/hbaseres/HBase.java
+++ src/main/java/cn/hassan/hbaseres/HBase.java
@@ -1,17 +1,16 @@
 package cn.hassan.hbaseres;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.HBaseConfiguration;
-import org.apache.hadoop.hbase.HColumnDescriptor;
-import org.apache.hadoop.hbase.HTableDescriptor;
-import org.apache.hadoop.hbase.TableName;
-import org.apache.hadoop.hbase.client.Admin;
-import org.apache.hadoop.hbase.client.Connection;
-import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.*;
+import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created with idea
@@ -36,6 +35,71 @@ public class HBase {
 		}
 	}
 
+	@After
+	public void after() {
+		try {
+			connection.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void deleteTest() throws IOException {
+		Table table = connection.getTable(TableName.valueOf("test"));
+		Delete delete = new Delete("1002".getBytes());
+		//在delete对象中指定要删除的列族-列名称
+		//delete.addColumn("cf".getBytes(), "name".getBytes());
+		table.delete(delete);
+	}
+
+	@Test
+	public void scanTest() throws IOException {
+		Table table = connection.getTable(TableName.valueOf("test"));
+		Scan scan = new Scan();
+		ResultScanner scanner = table.getScanner(scan);
+		Iterator<Result> iterator = scanner.iterator();
+		while (iterator.hasNext()) {
+			Result next = iterator.next();
+			List<Cell> cells = next.listCells();
+			printValue(cells);
+		}
+	}
+
+	@Test
+	public void getTest() throws IOException {
+		Table table = connection.getTable(TableName.valueOf("test"));
+		Get get = new Get("1000".getBytes());
+		Result result = table.get(get);
+		List<Cell> cells = result.listCells();
+		printValue(cells);
+	}
+
+	private void printValue(List<Cell> cells) {
+		for (Cell cell : cells) {
+			byte[] rowArray = cell.getRowArray();
+			byte[] familyArray = cell.getFamilyArray();
+			byte[] qualifierArray = cell.getQualifierArray();
+			byte[] valueArray = cell.getValueArray();
+			//按指定位置截取，获取rowArray、familyArray、qualifierArray、valueArray
+			System.out.print(new String(rowArray, cell.getRowOffset(), cell.getRowLength()));
+			System.out.print(" "+new String(familyArray, cell.getFamilyOffset(), cell.getFamilyLength()));
+			System.out.print(":" + new String(qualifierArray, cell.getQualifierOffset(), cell.getQualifierLength()));
+			System.out.println(" " + new String(valueArray, cell.getValueOffset(), cell.getValueLength()));
+		}
+	}
+
+	@Test
+	public void addTest() throws IOException {
+		Table table = connection.getTable(TableName.valueOf("test"));
+
+		Put put = new Put(Bytes.toBytes("1002"));
+		put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("name"), Bytes.toBytes("tom"));
+		put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("sex"), Bytes.toBytes("male"));
+		put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("age"), Bytes.toBytes("25"));
+
+		table.put(put);
+	}
 
 	@Test
 	public void createTest() {
